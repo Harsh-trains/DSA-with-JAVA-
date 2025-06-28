4adeffd9ea99a89148fcb2b7f1af03e77e14f4d7
Jyoti-bujethiya <pichu221119@gmail.com>
2025-06-28 17:54:16 +0530

    update array initialization in Swap.java

--- lectures/08-arrays/code/src/com/kunal/Swap.java
+++ lectures/08-arrays/code/src/com/kunal/Swap.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 
 public class Swap {
     public static void main(String[] args) {
-        int[] arr = {1, 3, 23, 9, 18, 56};
+        int[] arr = {1, 3, 23, 9};
 //        swap(arr, 0, 4);
         reverse(arr);
         System.out.println(Arrays.toString(arr));
