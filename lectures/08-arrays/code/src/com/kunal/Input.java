2d7312bcdadce21786ee5a7647120a9a079824c6
Jyoti-bujethiya <pichu221119@gmail.com>
2025-06-28 17:49:09 +0530

    comment out unused array elements in Input.java

--- lectures/08-arrays/code/src/com/kunal/Input.java
+++ lectures/08-arrays/code/src/com/kunal/Input.java
@@ -11,8 +11,8 @@ public class Input {
         int[] arr = new int[5];
         arr[0] = 23;
         arr[1] = 45;
-        arr[2] = 233;
-        arr[3] = 543;
+        //arr[2] = 233;
+        //arr[3] = 543;
         arr[4] = 3;
         // [23, 45, 233, 543, 3]
         System.out.println(arr[3]);
